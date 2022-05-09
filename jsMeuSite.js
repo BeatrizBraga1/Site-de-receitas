6c8c027682eacf91b030ea8692c8c9a6bb3edc4a
Ana Braga <anabeatrizssb16@gmail.com>
2022-05-09 09:26:51 -0300

    site de receitas

---
+++ jsMeuSite.js
@@ -0,0 +1,42 @@
+
+// USADO NO BOTÃO RECEITAS, NA BARRA DE NAV E NO RODAPÉ
+function redirecionamento() {
+    window.location.replace("Receitas.html")
+}
+
+// USADO NO BOTÃO VOLTAR, NA BARRA DE NAV "HOME" E NO RODAPÉ "HOME"
+function redirecionamento1() {
+    window.location.replace("index.html")
+}
+
+//----------------------------PARA A PÁGINA INDIVIDUAL DE CADA RECEITA---------------------------------------------------------------
+function redirecionamento2() {
+    window.location.replace("receita1.html")
+}
+
+function redirecionamento3() {
+    window.location.replace("receita2.html")
+}
+
+function redirecionamento4() {
+    window.location.replace("receita3.html")
+}
+
+function redirecionamento5() {
+    window.location.replace("receita4.html")
+}
+
+function redirecionamento6() {
+    window.location.replace("receita5.html")
+}
+
+function redirecionamento7() {
+    window.location.replace("receita6.html")
+}
+
+
+//--------------------------------BOTAO "VOLTAR" DA PAGINA DE CADA RECEITA ------------------------------------------------------------------------------------
+
+function redirecionamento8() {
+    window.location.replace("Receitas.html")
+}
